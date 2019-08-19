c19939f736a322cf282d35046110566d5776980d
gaetanok <ok.gaetan@gmail.com>
2019-08-19 18:02:14 +0200

    add new to island

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -2,8 +2,12 @@ class IslandsController < ApplicationController
   def index
     @islands = Island.all
   end
-  
+
   def show
     @island = Island.find(params[:id])
+  end
+
+  def new
+    @island = Island.new
   end
 end
