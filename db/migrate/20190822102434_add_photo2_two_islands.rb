0982d4b7b24aa0b15f6805cc873e059fce20554b
Pierrick Delage <pierrick.delage@gmail.com>
2019-08-22 12:26:32 +0200

    2 mores photos added to Island db

---
+++ db/migrate/20190822102434_add_photo2_two_islands.rb
@@ -0,0 +1,4 @@
+class AddPhoto2TwoIslands < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_090725) do
+ActiveRecord::Schema.define(version: 2019_08_22_102554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
