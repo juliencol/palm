72fc108414fc773ab5900a378a29a9ec07bbff00
Julien Colombain <julien@colombain.com>
2019-08-23 10:59:21 +0200

    button call-to-action

---
+++ db/migrate/20190823084919_remove_updated_at_from_reservations.rb
@@ -0,0 +1,4 @@
+class RemoveUpdatedAtFromReservations < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_150623) do
+ActiveRecord::Schema.define(version: 2019_08_23_084919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,17 +18,17 @@ ActiveRecord::Schema.define(version: 2019_08_22_150623) do
   create_table "islands", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.string "booked"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
     t.string "photo"
     t.float "price_by_night"
-    t.string "phototwo"
-    t.string "photothree"
+    t.boolean "booked", default: false
     t.float "latitude"
     t.float "longitude"
+    t.string "phototwo"
+    t.string "photothree"
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2019_08_22_150623) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.float "minimum_price"
+    t.float "maximum_price"
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
