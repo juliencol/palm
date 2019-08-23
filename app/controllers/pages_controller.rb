49c62043eb41477f89567d76474f26a75a581106
Julien Colombain <julien@colombain.com>
2019-08-23 14:32:39 +0200

    searchbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,18 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @islands = Island.all
-    @islands = Island.geocoded #returns flats with coordinates
-    @markers = @islands.map do |island|
-      {
-        lat: island.latitude,
-        lng: island.longitude
-      }
+    if params[:query].present?
+      @islands = Island.where(name: params[:query])
+    elsif
+      @islands = Island.geocoded #returns flats with coordinates
+      @markers = @islands.map do |island|
+        {
+          lat: island.latitude,
+          lng: island.longitude
+        }
+      end
+    else
+      @islands = Island.all
     end
   end
 end
